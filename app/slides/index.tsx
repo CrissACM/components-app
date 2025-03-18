41f91dcb1876e2fceccc9fa435c9db9c330a016b
Crissacm <alex.ccm7Z@gmail.com>
2025-03-18 14:23:49 -0400

    little change

--- app/slides/index.tsx
+++ app/slides/index.tsx
@@ -1,10 +1,88 @@
-import { View, Text } from 'react-native';
+import { ThemedButton } from '@/presentation/shared/ThemedButton';
+import { ThemedText } from '@/presentation/shared/ThemedText';
+import { ThemedView } from '@/presentation/shared/ThemedView';
+import {
+  FlatList,
+  Image,
+  useWindowDimensions,
+  type ImageSourcePropType,
+} from 'react-native';
 
-const SlidesScreen = () => {
+interface Slide {
+  title: string;
+  desc: string;
+  img: ImageSourcePropType;
+}
+
+const items: Slide[] = [
+  {
+    title: 'Titulo 1',
+    desc: 'Ea et eu enim fugiat sunt reprehenderit sunt aute quis tempor ipsum cupidatat et.',
+    img: require('../../assets/images/slides/slide-1.png'),
+  },
+  {
+    title: 'Titulo 2',
+    desc: 'Anim est quis elit proident magna quis cupidatat curlpa labore Lorem ea. Exercitation mollit velit in aliquip tempor occaecat dolor minim amet dolor enim cillum excepteur. ',
+    img: require('../../assets/images/slides/slide-2.png'),
+  },
+  {
+    title: 'Titulo 3',
+    desc: 'Ex amet duis amet nulla. Aliquip ea Lorem ea culpa consequat proident. Nulla tempor esse ad tempor sit amet Lorem. Velit ea labore aute pariatur commodo duis veniam enim.',
+    img: require('../../assets/images/slides/slide-3.png'),
+  },
+];
+
+export default function SlidesScreen() {
   return (
-    <View>
-      <Text>SlidesScreen</Text>
-    </View>
+    <ThemedView>
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.title}
+        renderItem={({ item }) => <SlideItem item={item} key={item.title} />}
+        horizontal
+        pagingEnabled
+      />
+      <ThemedButton className='absolute bottom-10 right-5 w-[150px]'>
+        Siguientes
+      </ThemedButton>
+      <ThemedButton className='absolute bottom-10 right-5 w-[150px]'>
+        Finalizar
+      </ThemedButton>
+    </ThemedView>
+  );
+}
+interface SlideItemProps {
+  item: Slide;
+}
+
+const SlideItem = ({ item }: SlideItemProps) => {
+  const { width } = useWindowDimensions();
+  const { img, title, desc } = item;
+
+  return (
+    <ThemedView
+      className='justify-center flex-1 p-10 bg-red-500 rounded'
+      style={{ width: width }}
+    >
+      <Image
+        source={img}
+        style={{
+          width: width * 0.7,
+          height: width * 0.7,
+          resizeMode: 'center',
+          alignSelf: 'center',
+        }}
+      />
+
+      <ThemedText
+        type='h1'
+        className='text-light-primary dark:text-dark-primary'
+      >
+        {title}
+      </ThemedText>
+      <ThemedText type='h2' className='mt-10'>
+        {desc}
+      </ThemedText>
+    </ThemedView>
   );
 };
-export default SlidesScreen;
